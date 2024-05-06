1ce39a736ab01576dc7dc6716b8857fcb2d39014
Vera869 <verea8910@gmail.com>
2024-05-06 20:27:18 +0700

    +imgURL

--- vite-project/src/components/weatherBox/WeatherComponent.jsx
+++ vite-project/src/components/weatherBox/WeatherComponent.jsx
@@ -4,34 +4,22 @@ import styles from './WeatherComponent.module.css';
 function WeatherBox() {
    const cityName = useSelector((state) => state.weather.cityName);
    const dailyData = useSelector((state) => state.weather.cityWeather);
-
-   // let imgPath = '';
-   // if(weather === 'Clouds') {
-   //    imgPath = '/img/cloud_big.png';
-   // } else if(weather === 'Clear') {
-   //    imgPath = '/img/sunshine_big.png';
-   // } else if(weather === 'Drizzle') {
-   //    imgPath = '/img/sunshine_big.png';
-   // } else if(weather === 'Rain') {
-   //    imgPath = '/img/rain_big.png';
-   // } else if(weather === 'Mist') {
-   //    imgPath = '/img/sunshine_big.png';
-   // }
   return (
     <div className={styles.weather}>
       <h3 className={styles.weather_header}> {cityName} прогноз на сегодня и ближайшие 5 дней</h3>
       <ul className={styles.weather_content}>
          {dailyData.map((weather, index) => {
+            const iconcode = weather.weather[0].icon;
+            const imgURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
             const day = (weather.dt_txt).slice(8, 11) ;
             const month = (weather.dt_txt).slice(5, 7) ;
             const date = day +"."+ month;
             const ms = weather.dt * 1000;
             const weekdayName = new Date(ms).toLocaleString('ru', {weekday: 'long'});
-            //console.log(weekdayName);
             return( <li className={styles.content_item} key={index}>
                   <p className={styles.item_header}>{weekdayName}</p>
                   <p className={styles.item_header}>{date}</p>
-                  <img src={"/img/sunshine_big.png"} className={styles.item_img}/>
+                  <img src={imgURL} className={styles.item_img}/>
                   <p className={styles.item_header}>Температура</p>
                   <p className={styles.item_data}>{Math.round(weather.main.temp)}&deg;C</p>
                   <p className={styles.item_header}>Скорость ветра</p>
