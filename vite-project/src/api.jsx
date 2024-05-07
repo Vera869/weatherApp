e74cccf65cbc0bb4b84e0a1398396217199b3d41
Vera869 <verea8910@gmail.com>
2024-05-07 12:10:06 +0700

    +api Errors

--- vite-project/src/api.jsx
+++ vite-project/src/api.jsx
@@ -2,12 +2,17 @@ const apiKey = "585bf75429eb808608a0d73471d3b0a2";
 const host = 'https://api.openweathermap.org/data/2.5/forecast?';
 
 export async function getWeatherCity(city){
-   const response = await fetch(`${host}q=${city}&appid=${apiKey}&units=metric&lang=ru`)
-   if(!response.ok) {
-      throw new Error("Произошла ошибка");
-   } 
+   const response = await fetch(`${host}q=${city}&appid=${apiKey}&units=metric&lang=ru`);
+   // if(!response.ok) {
+   //    throw new Error("Произошла ошибка");
+   // } 
    if(response.status === 404) {
       throw new Error("Произошла ошибка Город не найден");
+   }
+   if(response.status === 500) {
+      throw new Error("Произошла ошибка, сервер сломался");
+   // }if(response.status === 404) {
+   //    throw new Error("Произошла ошибка Город не найден");
    }
     const res = response.json();
    return res
