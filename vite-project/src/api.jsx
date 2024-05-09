4e6e8bf4c02a1b7648542c72ce16c47ed1aa0f48
Vera869 <verea8910@gmail.com>
2024-05-09 16:28:42 +0700

    + build

--- vite-project/src/api.jsx
+++ vite-project/src/api.jsx
@@ -1,8 +1,8 @@
 const apiKey = "585bf75429eb808608a0d73471d3b0a2";
-const host = 'https://api.openweathermap.org/data/2.5/forecast?';
+const host = 'https://api.openweathermap.org/data/2.5/';
 
 export async function getWeatherCity(city){
-   const response = await fetch(`${host}q=${city}&appid=${apiKey}&units=metric&lang=ru`);
+   const response = await fetch(`${host}forecast?q=${city}&appid=${apiKey}&units=metric&lang=ru`);
    if(response.status === 404) {
       throw new Error("Город не найден");
    }
