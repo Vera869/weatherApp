7dae19773409dd0f7379d4d9b635b62ef89af5f6
Vera869 <verea8910@gmail.com>
2025-02-26 11:07:24 +0700

    +

--- vite-project/src/components/form/FormComponent.jsx
+++ vite-project/src/components/form/FormComponent.jsx
@@ -3,7 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { getWeatherCity } from "../../api";
 import { handleCityChange } from "../../utils/ValidationForm";
 import { useState } from "react";
-import { setApiError, setCityName, setCityWeather, setIsLoading } from "../../store/WeatherSlice";
+import {
+  setApiError,
+  setCityName,
+  setCityWeather,
+  setIsLoading,
+} from "../../store/WeatherSlice";
 
 const GetCityAtForm = () => {
   const dispatch = useDispatch();
@@ -11,36 +16,58 @@ const GetCityAtForm = () => {
   const cityName = useSelector((state) => state.weather.cityName);
   const isLoading = useSelector((state) => state.weather.isLoading);
   const isApiError = useSelector((state) => state.weather.apiError);
-   
+
   const searchHeandler = (event) => {
     // делает завпрос в апи  и отправляет ответ в стор через диспатч
     event.preventDefault();
-    if(!cityName) return;
-    else{
+    if (!cityName) return;
+    else {
+      dispatch(setIsLoading(true));
       dispatch(setApiError(""));
       dispatch(setCityWeather([]));
-      dispatch(setIsLoading(true));
-      getWeatherCity(cityName).then(data => {
-        setCityError([]);
-        const dailyData = data.list.filter(reading => reading.dt_txt.includes("12:00:00"));
-        dispatch(setCityWeather(dailyData));
-        dispatch(setIsLoading(false));
-      }).catch(error => {
-        dispatch(setIsLoading(false));
-        dispatch(setApiError(error.message));
-      });}
+
+      getWeatherCity(cityName)
+        .then((data) => {
+          setCityError([]);
+          const dailyData = data.list.filter((reading) =>
+            reading.dt_txt.includes("12:00:00")
+          );
+          dispatch(setCityWeather(dailyData));
+          dispatch(setIsLoading(false));
+        })
+        .catch((error) => {
+          dispatch(setIsLoading(false));
+          dispatch(setApiError(error.message));
+        });
+    }
   };
-  return(
+  return (
     <form className={s.container}>
-      <h2 className={s.header}>Выберите город</h2>
-      <input className={s.Input} type="text" name="city" placeholder="Город" value={cityName} 
-        onChange={(event) => {
-          handleCityChange(event, setCityError);
-          dispatch(setCityName(event.target.value));
-        }}/>
-      {isCityError? <p className={s.Input_errors}>{isCityError}</p>: ""}
-      <button className={s.Button} onClick={searchHeandler}disabled={isLoading}>{isLoading ? "Идёт поиск" : "Узнать погоду"}</button>
-      {isApiError? <p className={s.Input_errors}>{isApiError}</p>: ""}
+      <div className={s.inputBox}>
+        <input
+          className={s.Input}
+          type="text"
+          name="city"
+          placeholder="Город"
+          value={cityName}
+          onChange={(event) => {
+            handleCityChange(event, setCityError);
+            dispatch(setCityName(event.target.value));
+          }}
+        />
+        {isCityError ? <p className={s.Input_errors}>{isCityError}</p> : ""}
+      </div>
+
+      <div className={s.btnBox}>
+        <button
+          className={s.Button}
+          onClick={searchHeandler}
+          disabled={isLoading}
+        >
+          {isLoading ? "Идёт поиск" : "Узнать погоду"}
+        </button>
+        {isApiError ? <p className={s.Input_errors}>{isApiError}</p> : ""}
+      </div>
     </form>
   );
 };
--- vite-project/src/components/weatherBox/WeatherComponent.jsx
+++ vite-project/src/components/weatherBox/WeatherComponent.jsx
@@ -6,43 +6,53 @@ const WeatherBox = () => {
   const dailyData = useSelector((state) => state.weather.cityWeather);
   const isApiError = useSelector((state) => state.weather.apiError);
   const theme = useSelector((state) => state.weather.changeTheme);
-   
+
   return (
-    <div className={theme? s.conteiner : s.conteiner_dark }>
-      {dailyData.length <= 0 || isApiError ? "" :<div>
-        <h3 className={s.header}> {cityName} прогноз на 5 дней</h3>
-        <ul className={s.content}>
-          {dailyData.map((weather, index) => {
-            const iconcode = weather.weather[0].icon;
-            const imgURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
-            const day = (weather.dt_txt).slice(8, 11) ;
-            const month = (weather.dt_txt).slice(5, 7) ;
-            const date = day +"."+ month;
-            const ms = weather.dt * 1000;
-            const weekdayName = new Date(ms).toLocaleString("ru", {weekday: "long"});
-            return( <li className={s.item_conteiner} key={index}>
-              <p className={s.item_header}>{weekdayName}</p>
-              <p className={s.item_header}>{date}</p>
-              <img src={imgURL} className={s.item_img}/>
-              <div className={s.item_box}>
-                <img src='/img/term.png' className={s.item_icon}/>
-                <p className={s.item_data}>{Math.round(weather.main.temp)}&deg;C</p>
-              </div>
-              <div className={s.item_box}>
-                <img src='/img/wind.png' className={s.item_icon}/>
-                <p className={s.item_data}>{(weather.wind.speed).toFixed(1)} м/с</p>
-              </div>
-              <div className={s.item_box}>
-                <img src='/img/humidity.png' className={s.item_icon}/>
-                <p className={s.item_data}>{weather.main.humidity}%</p>
-              </div>
-            </li>
-            );
-          }) 
-          }
-        </ul>
-      </div>}
-     
+    <div className={theme ? s.conteiner : s.conteiner_dark}>
+      {dailyData.length <= 0 || isApiError ? (
+        ""
+      ) : (
+        <div>
+          <h3 className={s.header}> {cityName.toUpperCase()} прогноз на 5 дней</h3>
+          <ul className={s.content}>
+            {dailyData.map((weather, index) => {
+              const iconcode = weather.weather[0].icon;
+              const imgURL =
+                "http://openweathermap.org/img/w/" + iconcode + ".png";
+              const day = weather.dt_txt.slice(8, 11);
+              const month = weather.dt_txt.slice(5, 7);
+              const date = day + "." + month;
+              const ms = weather.dt * 1000;
+              const weekdayName = new Date(ms).toLocaleString("ru", {
+                weekday: "long",
+              });
+              return (
+                <li className={s.item_conteiner} key={index}>
+                  <p className={s.item_header}>{weekdayName}</p>
+                  <p className={s.item_header}>{date}</p>
+                  <img src={imgURL} className={s.item_img} />
+                  <div className={s.item_box}>
+                    <img src="/img/term.png" className={s.item_icon} />
+                    <p className={s.item_data}>
+                      {Math.round(weather.main.temp)}&deg;C
+                    </p>
+                  </div>
+                  <div className={s.item_box}>
+                    <img src="/img/wind.png" className={s.item_icon} />
+                    <p className={s.item_data}>
+                      {weather.wind.speed.toFixed(1)} м/с
+                    </p>
+                  </div>
+                  <div className={s.item_box}>
+                    <img src="/img/humidity.png" className={s.item_icon} />
+                    <p className={s.item_data}>{weather.main.humidity}%</p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
