2ebbed3758cffcf41fbbad17cf15347165e8d3d4
Vera869 <verea8910@gmail.com>
2025-02-28 12:37:43 +0700

    +todayCard

--- vite-project/src/components/todayCard/TodayCardComponent.jsx
+++ vite-project/src/components/todayCard/TodayCardComponent.jsx
@@ -3,6 +3,7 @@ import s from "./TodayCardComponent.module.css";
 
 const TodayCard = () => {
   const todayData = useSelector((state) => state.weather.todayWeather);
+  //   const cityName = useSelector((state) => state.weather.titleCity);
   //   console.log(todayData);
   //   const date = new Date().toISOString().slice(0, 10);
   const day = todayData[0].dt_txt.slice(8, 11);
@@ -15,40 +16,34 @@ const TodayCard = () => {
   return (
     <div className={s.card}>
       <div className={s.header}>
+        {/* {cityName.toUpperCase()} */}
+        <p className={s.header_item}>Сегодня</p>
         <p className={s.header_item}>{date}</p>
         <p className={s.header_item}>{weekdayName}</p>
       </div>
       <ul className={s.content}>
         {todayData.map((weather, index) => {
-         //  console.log(weather.dt_txt.slice(0, 10));
           const iconcode = weather.weather[0].icon;
           const imgURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
-          const day = weather.dt_txt.slice(8, 11);
-          const month = weather.dt_txt.slice(5, 7);
-          const date = day + "." + month;
-          const ms = weather.dt * 1000;
-          const weekdayName = new Date(ms).toLocaleString("ru", {
-            weekday: "long",
-          });
+          const time = weather.dt_txt.slice(11, 16);
           return (
-            <li className={s.item_conteiner} key={index}>
-              <p className={s.item_header}>{weekdayName}</p>
-              <p className={s.item_header}>{date}</p>
+            <li className={s.content_item} key={index}>
+              <p className={s.item_header}>{time}</p>
               <img src={imgURL} className={s.item_img} />
               <div className={s.item_box}>
-                <img src="/img/term.png" className={s.item_icon} />
+                {/* <img src="/img/term.png" className={s.item_icon} /> */}
                 <p className={s.item_data}>
                   {Math.round(weather.main.temp)}&deg;C
                 </p>
               </div>
               <div className={s.item_box}>
-                <img src="/img/wind.png" className={s.item_icon} />
+                {/* <img src="/img/wind.png" className={s.item_icon} /> */}
                 <p className={s.item_data}>
-                  {weather.wind.speed.toFixed(1)} м/с
+                  {weather.wind.speed.toFixed(1)}м/с
                 </p>
               </div>
               <div className={s.item_box}>
-                <img src="/img/humidity.png" className={s.item_icon} />
+                {/* <img src="/img/humidity.png" className={s.item_icon} /> */}
                 <p className={s.item_data}>{weather.main.humidity}%</p>
               </div>
             </li>
